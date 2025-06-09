9497e80b1ccc81620a3e43693ff287b406744ed5
jiwon Lee <dlwleasy1@gmail.com>
2025-06-09 21:13:42 +0900

    signup 기능추가

---
+++ src/main/java/jobstack/jobstack/SecurityConfig.java
@@ -0,0 +1,56 @@
+package jobstack.jobstack;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+//@Configuration
+//@EnableWebSecurity
+//public class SecurityConfig {
+//    @Bean
+//    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+//        http
+//                .csrf(csrf -> csrf.disable())
+//                .authorizeHttpRequests(authorize ->
+//                        authorize
+//                                .requestMatchers("/**").permitAll()
+//
+//
+//
+//
+//
+//                );
+//
+//        ;
+//        return http.build();
+//    }
+//}
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf
+                        .ignoringRequestMatchers("/h2-console/**")  // CSRF 예외
+                )
+                .headers(headers -> headers
+                        .frameOptions(frame -> frame.sameOrigin())  // iframe 허용
+                )
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/h2-console/**").permitAll()  // 허용
+                        .requestMatchers("/**").permitAll()             // 나머지도 허용
+                );
+        return http.build();
+    }
+    @Bean
+    PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+}
---
+++ src/main/java/jobstack/jobstack/User/UserRepository.java
@@ -0,0 +1,6 @@
+package jobstack.jobstack.User;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<SiteUser, Long> {
+}
