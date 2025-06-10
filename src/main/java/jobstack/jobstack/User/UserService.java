30890f2575340a54de1f017a74685a6aef14a73a
jiwon Lee <dlwleasy1@gmail.com>
2025-06-10 22:54:52 +0900

    signup 기능추가

--- src/main/java/jobstack/jobstack/User/UserService.java
+++ src/main/java/jobstack/jobstack/User/UserService.java
@@ -1,13 +1,19 @@
 package jobstack.jobstack.User;
 
+import jobstack.jobstack.DataNotFoundException;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service
-public class UserService {
+public class UserService  {
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
 
@@ -15,10 +21,10 @@ public class UserService {
         SiteUser user = new SiteUser();
         user.setUsername(username);
         user.setEmail(email);
-        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
         user.setPassword(passwordEncoder.encode(password));
-
         this.userRepository.save(user);
         return user;
     }
+
+
 }
